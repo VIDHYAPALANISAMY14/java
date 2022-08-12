3abd5de59a8bd467deb65b8171c32c50aea32ee9
VIDHYA <105865700+VIDHYAPALANISAMY14@users.noreply.github.com>
2022-08-12 23:03:01 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+ public class helloworld extends Applet{
+   public void paint(Graphics g){
+     g.drawString("Hello world" 150,150);
+   }
+ }
